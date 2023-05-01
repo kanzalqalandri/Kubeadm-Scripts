b4fb64fd343d1910d30de5c22863dd0f12592672
Kanzal Qalandri <128204757+kanzalqalandri@users.noreply.github.com>
2023-05-01 13:51:01 +0500

    kubeadm master script

---
+++ k8s-master.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+sudo swapoff -a
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+sudo apt update
+sudo apt full-upgrade -y
+
+#---------------------
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+#---------------------
+sudo modprobe overlay
+sudo modprobe br_netfilter
+#--------------------------
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward = 1
+EOF
+#--------------------
+sudo sysctl --system
+#-------------
+sudo mkdir /etc/apt/keyrings
+sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+sudo echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+#-----------
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl 
+sudo apt install -y kubelet kubeadm kubelet docker.io
+sudo apt-mark hold kubelet kubeadm kubectl docker.io
+sudo mkdir /etc/containerd
+sudo sh -c "containerd config default > /etc/containerd/config.toml"
+sudo sed -i 's/            SystemdCgroup = false/            SystemdCgroup = true/' /etc/containerd/config.toml
+#-----------
+sudo systemctl restart containerd.service
+sudo systemctl restart kubelet.service
+sudo systemctl start docker.service
+sudo systemctl enable kubelet.service
+sudo systemctl enable docker.service
+#----------------------
+sudo kubeadm config images pull
+sudo kubeadm init --pod-network-cidr=10.0.0.0/16 #--kubernetes-version=1.25.9 --ignore-preflight-errors=all
+#------------------
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+#------------
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.1/manifests/tigera-operator.yaml
+curl https://raw.githubusercontent.com/projectcalico/calico/v3.25.1/manifests/custom-resources.yaml -O
+sed -i 's/cidr: 192\.168\.0\.0\/16/cidr: 10.0.0.0\/16/g' custom-resources.yaml
+kubectl create -f custom-resources.yaml
+#-----------
+watch kubectl get pods -A
+
