79f5e5d98f5b3af9add74fa34a3bad6f78a6ed70
Kanzal Qalandri <kanzalqalandri@gmail.com>
2023-06-05 22:39:07 +0500

    master file updated after gpg key update.

--- k8s-master.sh
+++ k8s-master.sh
@@ -25,10 +25,12 @@ sudo sysctl --system
 sudo mkdir /etc/apt/keyrings
 sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 sudo echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo curl -fsSL "https://packages.cloud.google.com/apt/doc/apt-key.gpg" | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/kubernetes-archive-keyring.gpg
+sudo sh -c "echo 'deb https://packages.cloud.google.com/apt kubernetes-xenial main' > /etc/apt/sources.list.d/kubernetes.list"
 #-----------
 sudo apt update
 sudo apt install -y apt-transport-https ca-certificates curl 
-sudo apt install -y kubelet kubeadm kubelet docker.io
+sudo apt install -y kubelet=1.26.5-00 kubeadm=1.26.5-00 kubectl=1.26.5-00 docker.io
 sudo apt-mark hold kubelet kubeadm kubectl docker.io
 sudo mkdir /etc/containerd
 sudo sh -c "containerd config default > /etc/containerd/config.toml"
@@ -41,7 +43,7 @@ sudo systemctl enable kubelet.service
 sudo systemctl enable docker.service
 #----------------------
 sudo kubeadm config images pull
-sudo kubeadm init --pod-network-cidr=10.0.0.0/16 #--kubernetes-version=1.25.9 --ignore-preflight-errors=all
+sudo kubeadm init --pod-network-cidr=10.10.0.0/16
 #------------------
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
@@ -49,8 +51,7 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 #------------
 kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.1/manifests/tigera-operator.yaml
 curl https://raw.githubusercontent.com/projectcalico/calico/v3.25.1/manifests/custom-resources.yaml -O
-sed -i 's/cidr: 192\.168\.0\.0\/16/cidr: 10.0.0.0\/16/g' custom-resources.yaml
+sed -i 's/cidr: 192\.168\.0\.0\/16/cidr: 10.10.0.0\/16/g' custom-resources.yaml
 kubectl create -f custom-resources.yaml
 #-----------
 watch kubectl get pods -A
-
