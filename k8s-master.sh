f2f9055bb39edd675b20e867f2d18f1f47d237f9
Kanzal Qalandri <kanzalqalandri@gmail.com>
2023-06-11 02:18:09 +0500

    updated master and worker delete.

--- k8s-master.sh
+++ k8s-master.sh
@@ -3,7 +3,7 @@
 sudo swapoff -a
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 sudo apt update
-sudo apt full-upgrade -y
+#sudo apt full-upgrade -y
 
 #---------------------
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
@@ -21,15 +21,14 @@ net.ipv4.ip_forward = 1
 EOF
 #--------------------
 sudo sysctl --system
-#-------------
-sudo mkdir /etc/apt/keyrings
-sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-sudo echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo curl -fsSL "https://packages.cloud.google.com/apt/doc/apt-key.gpg" | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/kubernetes-archive-keyring.gpg
-sudo sh -c "echo 'deb https://packages.cloud.google.com/apt kubernetes-xenial main' > /etc/apt/sources.list.d/kubernetes.list"
 #-----------
 sudo apt update
-sudo apt install -y apt-transport-https ca-certificates curl 
+sudo apt install -y apt-transport-https ca-certificates curl
+#-----------
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+#--------
+sudo apt-get update
 sudo apt install -y kubelet=1.26.5-00 kubeadm=1.26.5-00 kubectl=1.26.5-00 docker.io
 sudo apt-mark hold kubelet kubeadm kubectl docker.io
 sudo mkdir /etc/containerd
