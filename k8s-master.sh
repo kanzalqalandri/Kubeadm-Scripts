1ad520480dbfe9a4558a31eb58a8c57b6118b4d4
Kanzal Qalandri <kanzalqalandri@gmail.com>
2023-07-16 15:25:59 +0500

    Updated scripts.

--- k8s-master.sh
+++ k8s-master.sh
@@ -1,54 +1,82 @@
 #!/bin/bash
 
+
+
+# Update Everything
+sudo apt update
+sudo apt full-upgrade -y
+
+
+# Turn Swap Off
 sudo swapoff -a
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
-sudo apt update
-#sudo apt full-upgrade -y
 
-#---------------------
-cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+
+# Configure persistant modules
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
 overlay
 br_netfilter
 EOF
-#---------------------
+
+# Load persistant modules
 sudo modprobe overlay
 sudo modprobe br_netfilter
-#--------------------------
+
+# Configure sysctl
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-iptables = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward = 1
 EOF
-#--------------------
+
+# Reload sysctl
 sudo sysctl --system
-#-----------
-sudo apt update
-sudo apt install -y apt-transport-https ca-certificates curl
-#-----------
-curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-#--------
+
+# Containerd Installation and Setup
 sudo apt-get update
-sudo apt install -y kubelet=1.26.5-00 kubeadm=1.26.5-00 kubectl=1.26.5-00 docker.io
-sudo apt-mark hold kubelet kubeadm kubectl docker.io
+sudo apt-get install ca-certificates curl gnupg
+
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install containerd.io -y
+
 sudo mkdir /etc/containerd
 sudo sh -c "containerd config default > /etc/containerd/config.toml"
 sudo sed -i 's/            SystemdCgroup = false/            SystemdCgroup = true/' /etc/containerd/config.toml
-#-----------
 sudo systemctl restart containerd.service
+
+# Install k8s Pakages
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl
+
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+sudo apt install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl containerd.io
+
 sudo systemctl restart kubelet.service
 sudo systemctl enable kubelet.service
-#----------------------
+
+# Pull Kubeadm Images
 sudo kubeadm config images pull
+
+# Initialize Container
 sudo kubeadm init --pod-network-cidr=10.10.0.0/16
-#------------------
+
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
-#------------
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.1/manifests/tigera-operator.yaml
-curl https://raw.githubusercontent.com/projectcalico/calico/v3.25.1/manifests/custom-resources.yaml -O
+
+# Install calico CNI
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/tigera-operator.yaml
+curl https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/custom-resources.yaml -O
 sed -i 's/cidr: 192\.168\.0\.0\/16/cidr: 10.10.0.0\/16/g' custom-resources.yaml
 kubectl create -f custom-resources.yaml
-#-----------
+
+# Check Cluster
 watch kubectl get pods -A
--- k8s-worker.sh
+++ k8s-worker.sh
@@ -1,40 +1,63 @@
 #!/bin/bash
 
+
+
+# Update Everything
+sudo apt update
+sudo apt full-upgrade -y
+
+
+# Turn Swap Off
 sudo swapoff -a
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
-sudo apt update
-#sudo apt full-upgrade -y
 
-#---------------------
-cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+
+# Configure persistant modules
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
 overlay
 br_netfilter
 EOF
-#---------------------
+
+# Load persistant modules
 sudo modprobe overlay
 sudo modprobe br_netfilter
-#--------------------------
+
+# Configure sysctl
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-iptables = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward = 1
 EOF
-#--------------------
+
+# Reload sysctl
 sudo sysctl --system
-#-----------
-sudo apt update
-sudo apt install -y apt-transport-https ca-certificates curl
-#-----------
-curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-#--------
+
+# Containerd Installation and Setup
 sudo apt-get update
-sudo apt install -y kubelet=1.26.5-00 kubeadm=1.26.5-00 kubectl=1.26.5-00 docker.io
-sudo apt-mark hold kubelet kubeadm kubectl docker.io
+sudo apt-get install ca-certificates curl gnupg
+
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install containerd.io -y
+
 sudo mkdir /etc/containerd
 sudo sh -c "containerd config default > /etc/containerd/config.toml"
 sudo sed -i 's/            SystemdCgroup = false/            SystemdCgroup = true/' /etc/containerd/config.toml
-#-----------
 sudo systemctl restart containerd.service
+
+# Install k8s Pakages
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl
+
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+sudo apt install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl containerd.io
+
 sudo systemctl restart kubelet.service
 sudo systemctl enable kubelet.service
